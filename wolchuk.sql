2f22c5209120607050c813701a0efa814f7a39ff
lucascasadei <89543116+lucascasadei@users.noreply.github.com>
2024-11-08 21:54:40 -0300

    db update

--- wolchuk.sql
+++ wolchuk.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 07-11-2024 a las 19:20:06
+-- Tiempo de generación: 09-11-2024 a las 01:53:48
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -39,6 +39,26 @@ CREATE TABLE `devoluciones` (
   `fecha` date NOT NULL,
   `hora` datetime(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `stock`
+--
+
+CREATE TABLE `stock` (
+  `idStock` int(11) NOT NULL,
+  `codBarras` varchar(50) NOT NULL,
+  `descripcion` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `stock`
+--
+
+INSERT INTO `stock` (`idStock`, `codBarras`, `descripcion`) VALUES
+(1, '7798085681643', 'leche'),
+(2, '44444', 'pan');
 
 -- --------------------------------------------------------
 
@@ -95,7 +115,7 @@ CREATE TABLE `usuarios` (
   `apellido` varchar(50) NOT NULL,
   `usuario` varchar(50) NOT NULL,
   `correo` varchar(50) NOT NULL,
-  `contrasena` varchar(15) NOT NULL,
+  `contrasena` varchar(200) NOT NULL,
   `descripcion` varchar(50) NOT NULL,
   `idTipoUsuario` int(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -105,8 +125,17 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`idUsuario`, `nombre`, `apellido`, `usuario`, `correo`, `contrasena`, `descripcion`, `idTipoUsuario`) VALUES
-(1, 'lucas', 'casadei', 'lucaa', 'lucas@gmail.com', 'lucas', 'lukaaaaaaaaaaa', 1),
-(2, '', '', '', 'brisa@gmail.com', 'brisa', '', 1);
+(6, '', '', 'fg', '', '$2y$10$Op.fNY2CZVPS03BF9ptOpe/ESg0yB7vp8ptW65oa5fi', '', 1),
+(7, '', '', '236', '', '$2y$10$W1lOB/Nen5.yAp9qpR7Eb.4y8UrZSnwAhOhlO/AFiIk', '', 1),
+(8, '', '', 'asd', '', '$2y$10$MQTRppC6uTNuRqZuvcTJm.6KfIk1Wxl2FNHJqI4M8QbIULHnLTRIa', '', 2),
+(9, '', '', 'asd22', '', '$2y$10$rcg8POEqsjVrH/s0dorlvObdTTXDJS/wbHokeGthnirpwy.q7gFX.', '', 2),
+(10, '', '', 'bri3411  ', '', '$2y$10$NVRz9zRH4aZixRAkKf1hX.EtkC0p3WaoMKIKt9ATYKLTQp1ApPeUm', '', 1),
+(11, '', '', 'gdfh', '', '$2y$10$4RpqeKCN1YazkmhSSB5jq.uIUmaGhMd7l7.TxHryy9GoaBFU/u9SG', '', 2),
+(12, '', '', 'asdwqeq', '', '$2y$10$6zLbMbQFO6c2e1YEZHJmT.pyumXD7yTbAGcG1Uqo.i34.jD1/02tS', '', 2),
+(13, '', '', 'dasas', '', '$2y$10$UP3YRYYtl8RBEytGrPTgcugu8eU9A3x2ih8ouJrLY4GWg2wk8Xzui', '', 2),
+(14, '', '', 'asddsd', '', '$2y$10$YbYb1OTrmYXKA2FKRzNP0urtBMG.yLfn1yEyYGS8WdQ.ej2HfZKee', '', 2),
+(15, '', '', 'bri', '', '$2y$10$XBqkWJpUuMeUmXLo/Z21D.KeNtu6GrYCr5HToKINlSeKfNf1Ki5lC', '', 1),
+(16, '', '', 'asd423', '', '$2y$10$a8PrTxvwg5hm0vooIbjIXuz/Pp64sFKOyhW3IaaIc4JTnKZd8Lu5C', '', 2);
 
 --
 -- Índices para tablas volcadas
@@ -120,6 +149,12 @@ ALTER TABLE `devoluciones`
   ADD KEY `idTipoDevolucion` (`idTipoDevolucion`),
   ADD KEY `idUsuario` (`idUsuario`),
   ADD KEY `idTipoDevolucion_2` (`idTipoDevolucion`);
+
+--
+-- Indices de la tabla `stock`
+--
+ALTER TABLE `stock`
+  ADD PRIMARY KEY (`idStock`);
 
 --
 -- Indices de la tabla `tipodevoluciones`
@@ -145,6 +180,12 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `stock`
+--
+ALTER TABLE `stock`
+  MODIFY `idStock` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT de la tabla `tipodevoluciones`
 --
 ALTER TABLE `tipodevoluciones`
@@ -160,7 +201,7 @@ ALTER TABLE `tipousuarios`
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `idUsuario` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `idUsuario` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- Restricciones para tablas volcadas
